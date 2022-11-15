4f944c463f265560a924889a58c8c1f454597370
Stanislav Kulakov <stanisla.kulakov0304@gmail.com>
2022-11-15 13:15:25 +0300

    Delet get and set, add tests

---
+++ src/main/java/Smartphone.java
@@ -0,0 +1,17 @@
+public class Smartphone extends Product {
+
+    protected String vendor;
+
+    public Smartphone(int id, String title, int price, String vendor) {
+        super(id, title, price);
+        this.vendor = vendor;
+    }
+
+//    public String getVendor() {
+    //       return vendor;
+    //  }
+
+    // public void setVendor(String vendor) {
+    //      this.vendor = vendor;
+    //  }
+}
---
+++ src/test/java/RepositoryTest.java
@@ -0,0 +1,76 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class RepositoryTest {
+    Book book1 = new Book(67, "War and Peace", 1500, "Lev Tolstoy");
+    Smartphone phone1 = new Smartphone(89, "Iphone", 9500, "Apple");
+    Book book2 = new Book(24, "Big debt crises", 2800, "Ray Dalio");
+    Smartphone phone2 = new Smartphone(99, "Honor", 8400, "HUAWEI");
+
+    @Test
+    public void addProducts() {
+        Repository repo = new Repository();
+        repo.addProduct(book1);
+        repo.addProduct(phone1);
+        repo.addProduct(book2);
+        repo.addProduct(phone2);
+        Product[] expected = {book1, phone1, book2, phone2};
+        Product[] actual = repo.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void searchProductText() {
+        Repository repo = new Repository();
+        Manager manager = new Manager(repo);
+        manager.add(book1);
+        manager.add(phone1);
+        manager.add(book2);
+        manager.add(phone2);
+        Product[] expected = {book1};
+        Product[] actual = manager.searchBy("War and Peace");
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void removeById() {
+        Repository repo = new Repository();
+        repo.addProduct(book1);
+        repo.addProduct(phone1);
+        repo.addProduct(book2);
+        repo.addProduct(phone2);
+        repo.removeById(67);
+        Product[] expected = {phone1, book2, phone2};
+        Product[] actual = repo.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void changeId() {
+        Repository repo = new Repository();
+        Manager manager = new Manager(repo);
+
+        manager.add(book1);
+        book1.setId(23);
+
+        int expected = 23;
+        int actual = book1.getId();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void searchPrice() {
+        Repository repo = new Repository();
+        Manager manager = new Manager(repo);
+
+        manager.add(phone2);
+        phone2.setPrice(8400);
+
+        int expected = 8400;
+        int actual = phone2.getPrice();
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
