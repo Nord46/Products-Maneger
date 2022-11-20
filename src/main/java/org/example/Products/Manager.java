d0b5c21bad7c260ea39ea5ed6be88dd2f6298573
Stanislav Kulakov <stanisla.kulakov0304@gmail.com>
2022-11-20 13:34:44 +0300

    100% coverage

--- src/main/java/org/example/Products/Manager.java
+++ src/main/java/org/example/Products/Manager.java
@@ -4,25 +4,21 @@ public class Manager {
     protected Repository repo;
 
     public Manager(Repository repo) {
+
         this.repo = repo;
     }
 
     public void add(Product product) {
+
         repo.addProduct(product);
     }
 
-    public void removeById(int id) {
-        repo.removeById(id);
-    }
 
     public Product[] searchBy(String text) {
         Product[] result = new Product[0];
         for (Product product : repo.findAll()) {
             if (matches(product, text)) {
                 Product[] tmp = new Product[result.length + 1];
-                for (int i = 0; i < result.length; i++) {
-                    tmp[i] = result[i];
-                }
                 tmp[tmp.length - 1] = product;
                 result = tmp;
             }
